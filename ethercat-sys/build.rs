256879f074d165823014467116374439a645c2b8
Georg Brandl <g.brandl@fz-juelich.de>
2018-10-15 09:17:19 +0200

    restrict generated binding code

--- ethercat-sys/build.rs
+++ ethercat-sys/build.rs
@@ -11,7 +11,14 @@ fn main() {
         .header("/home/gbr/devel/ext/ethercat-hg/lib/ioctl.h")
         .clang_arg("-I/home/gbr/devel/ext/ethercat-hg")
         .derive_default(true)
+        .derive_debug(false)
         .prepend_enum_name(false)
+        .ignore_functions()
+        .whitelist_type("ec_ioctl_.*")
+        .whitelist_type("ec_master_state_t")
+        .whitelist_var("EC_IOCTL_.*")
+        .whitelist_var("EC_MAX_.*")
+        .layout_tests(false)
         .generate()
         .expect("Unable to generate bindings");
 
