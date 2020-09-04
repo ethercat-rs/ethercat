410d2fbdfc6055d389e713b1fa109b395130e18f
Markus Kohlhase <markus.kohlhase@slowtec.de>
2020-09-04 16:18:13 +0200

    Update to EtherCAT Master v1.5.2-sncn-11

--- ethercat-sys/src/lib.rs
+++ ethercat-sys/src/lib.rs
@@ -13,6 +13,7 @@ pub mod ioctl {
     use super::EC_IOCTL_TYPE as EC;
     use super::*;
 
+    // Command-line tool
     ioctl!(read      MODULE               with EC, 0x00; ec_ioctl_module_t);
     ioctl!(read      MASTER               with EC, 0x01; ec_ioctl_master_t);
     ioctl!(readwrite SLAVE                with EC, 0x02; ec_ioctl_slave_t);
@@ -44,64 +45,101 @@ pub mod ioctl {
     ioctl!(readwrite CONFIG_SDO           with EC, 0x1c; ec_ioctl_config_sdo_t);
     ioctl!(readwrite CONFIG_IDN           with EC, 0x1d; ec_ioctl_config_idn_t);
     ioctl!(readwrite EOE_HANDLER          with EC, 0x1e; ec_ioctl_eoe_handler_t);
-    ioctl!(none      REQUEST              with EC, 0x1f);
-    ioctl!(none      CREATE_DOMAIN        with EC, 0x20);
-    ioctl!(readwrite CREATE_SLAVE_CONFIG  with EC, 0x21; ec_ioctl_config_t);
-    ioctl!(write     SELECT_REF_CLOCK     with EC, 0x22; u32);
-    ioctl!(read      ACTIVATE             with EC, 0x23; ec_ioctl_master_activate_t);
-    ioctl!(none      DEACTIVATE           with EC, 0x24);
-    ioctl!(arg       SEND                 with EC, 0x25);
-    ioctl!(none      RECEIVE              with EC, 0x26);
-    ioctl!(read      MASTER_STATE         with EC, 0x27; ec_master_state_t);
-    ioctl!(readwrite MASTER_LINK_STATE    with EC, 0x28; ec_ioctl_link_state_t);
-    ioctl!(write     APP_TIME             with EC, 0x29; ec_ioctl_app_time_t);
-    ioctl!(none      SYNC_REF             with EC, 0x2a);
-    ioctl!(none      SYNC_SLAVES          with EC, 0x2b);
-    ioctl!(read      REF_CLOCK_TIME       with EC, 0x2c; u32);
-    ioctl!(none      SYNC_MON_QUEUE       with EC, 0x2d);
-    ioctl!(read      SYNC_MON_PROCESS     with EC, 0x2e; u32);
-    ioctl!(none      RESET                with EC, 0x2f);
-    ioctl!(write     SC_SYNC              with EC, 0x30; ec_ioctl_config_t);
-    ioctl!(write     SC_WATCHDOG          with EC, 0x31; ec_ioctl_config_t);
-    ioctl!(write     SC_ADD_PDO           with EC, 0x32; ec_ioctl_config_pdo_t);
-    ioctl!(write     SC_CLEAR_PDOS        with EC, 0x33; ec_ioctl_config_pdo_t);
-    ioctl!(write     SC_ADD_ENTRY         with EC, 0x34; ec_ioctl_add_pdo_entry_t);
-    ioctl!(write     SC_CLEAR_ENTRIES     with EC, 0x35; ec_ioctl_config_pdo_t);
-    ioctl!(readwrite SC_REG_PDO_ENTRY     with EC, 0x36; ec_ioctl_reg_pdo_entry_t);
-    ioctl!(readwrite SC_REG_PDO_POS       with EC, 0x37; ec_ioctl_reg_pdo_pos_t);
-    ioctl!(write     SC_DC                with EC, 0x38; ec_ioctl_config_t);
-    ioctl!(write     SC_SDO               with EC, 0x39; ec_ioctl_sc_sdo_t);
-    ioctl!(write     SC_EMERG_SIZE        with EC, 0x3a; ec_ioctl_sc_emerg_t);
-    ioctl!(readwrite SC_EMERG_POP         with EC, 0x3b; ec_ioctl_sc_emerg_t);
-    ioctl!(write     SC_EMERG_CLEAR       with EC, 0x3c; ec_ioctl_sc_emerg_t);
-    ioctl!(readwrite SC_EMERG_OVERRUNS    with EC, 0x3d; ec_ioctl_sc_emerg_t);
-    ioctl!(readwrite SC_SDO_REQUEST       with EC, 0x3e; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite SC_REG_REQUEST       with EC, 0x3f; ec_ioctl_reg_request_t);
-    ioctl!(readwrite SC_VOE               with EC, 0x40; ec_ioctl_voe_t);
-    ioctl!(readwrite SC_STATE             with EC, 0x41; ec_ioctl_sc_state_t);
-    ioctl!(write     SC_IDN               with EC, 0x42; ec_ioctl_sc_idn_t);
-    ioctl!(arg       DOMAIN_SIZE          with EC, 0x43);
-    ioctl!(arg       DOMAIN_OFFSET        with EC, 0x44);
-    ioctl!(arg       DOMAIN_PROCESS       with EC, 0x45);
-    ioctl!(arg       DOMAIN_QUEUE         with EC, 0x46);
-    ioctl!(readwrite DOMAIN_STATE         with EC, 0x47; ec_ioctl_domain_state_t);
-    ioctl!(readwrite SDO_REQUEST_INDEX    with EC, 0x48; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite SDO_REQUEST_TIMEOUT  with EC, 0x49; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite SDO_REQUEST_STATE    with EC, 0x4a; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite SDO_REQUEST_READ     with EC, 0x4b; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite SDO_REQUEST_WRITE    with EC, 0x4c; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite SDO_REQUEST_DATA     with EC, 0x4d; ec_ioctl_sdo_request_t);
-    ioctl!(readwrite REG_REQUEST_DATA     with EC, 0x4e; ec_ioctl_reg_request_t);
-    ioctl!(readwrite REG_REQUEST_STATE    with EC, 0x4f; ec_ioctl_reg_request_t);
-    ioctl!(readwrite REG_REQUEST_WRITE    with EC, 0x50; ec_ioctl_reg_request_t);
-    ioctl!(readwrite REG_REQUEST_READ     with EC, 0x51; ec_ioctl_reg_request_t);
-    ioctl!(write     VOE_SEND_HEADER      with EC, 0x52; ec_ioctl_voe_t);
-    ioctl!(readwrite VOE_REC_HEADER       with EC, 0x53; ec_ioctl_voe_t);
-    ioctl!(write     VOE_READ             with EC, 0x54; ec_ioctl_voe_t);
-    ioctl!(write     VOE_READ_NOSYNC      with EC, 0x55; ec_ioctl_voe_t);
-    ioctl!(readwrite VOE_WRITE            with EC, 0x56; ec_ioctl_voe_t);
-    ioctl!(readwrite VOE_EXEC             with EC, 0x57; ec_ioctl_voe_t);
-    ioctl!(readwrite VOE_DATA             with EC, 0x58; ec_ioctl_voe_t);
-    ioctl!(write     SET_SEND_INTERVAL    with EC, 0x59; usize);
-    ioctl!(write     SC_OVERLAPPING_IO    with EC, 0x5a; ec_ioctl_config_t);
+
+    // Application interface
+    ioctl!(none      REQUEST             with EC, 0x1f);
+    ioctl!(none      CREATE_DOMAIN       with EC, 0x20);
+    ioctl!(readwrite CREATE_SLAVE_CONFIG with EC, 0x21; ec_ioctl_config_t);
+    ioctl!(write     SELECT_REF_CLOCK    with EC, 0x22; u32);
+    ioctl!(read      ACTIVATE            with EC, 0x23; ec_ioctl_master_activate_t);
+    ioctl!(none      DEACTIVATE          with EC, 0x24);
+    ioctl!(arg       SEND                with EC, 0x25);
+    ioctl!(none      RECEIVE             with EC, 0x26);
+    ioctl!(read      MASTER_STATE        with EC, 0x27; ec_master_state_t);
+    ioctl!(readwrite MASTER_LINK_STATE   with EC, 0x28; ec_ioctl_link_state_t);
+    ioctl!(write     APP_TIME            with EC, 0x29; u64);
+    ioctl!(none      SYNC_REF            with EC, 0x2a);
+    ioctl!(write     SYNC_REF_TO         with EC, 0x2b; u64);
+    ioctl!(none      SYNC_SLAVES         with EC, 0x2c);
+    ioctl!(read      REF_CLOCK_TIME      with EC, 0x2d; u32);
+    ioctl!(none      SYNC_MON_QUEUE      with EC, 0x2e);
+    ioctl!(read      SYNC_MON_PROCESS    with EC, 0x2f; u32);
+    ioctl!(none      RESET               with EC, 0x30);
+
+    ioctl!(write     SC_SYNC             with EC, 0x31; ec_ioctl_config_t);
+    ioctl!(write     SC_WATCHDOG         with EC, 0x32; ec_ioctl_config_t);
+    ioctl!(write     SC_ADD_PDO          with EC, 0x33; ec_ioctl_config_pdo_t);
+    ioctl!(write     SC_CLEAR_PDOS       with EC, 0x34; ec_ioctl_config_pdo_t);
+    ioctl!(write     SC_ADD_ENTRY        with EC, 0x35; ec_ioctl_add_pdo_entry_t);
+    ioctl!(write     SC_CLEAR_ENTRIES    with EC, 0x36; ec_ioctl_config_pdo_t);
+    ioctl!(readwrite SC_REG_PDO_ENTRY    with EC, 0x37; ec_ioctl_reg_pdo_entry_t);
+    ioctl!(readwrite SC_REG_PDO_POS      with EC, 0x38; ec_ioctl_reg_pdo_pos_t);
+    ioctl!(write     SC_DC               with EC, 0x39; ec_ioctl_config_t);
+    ioctl!(write     SC_SDO              with EC, 0x3a; ec_ioctl_sc_sdo_t);
+    ioctl!(write     SC_EMERG_SIZE       with EC, 0x3b; ec_ioctl_sc_emerg_t);
+    ioctl!(readwrite SC_EMERG_POP        with EC, 0x3c; ec_ioctl_sc_emerg_t);
+    ioctl!(write     SC_EMERG_CLEAR      with EC, 0x3d; ec_ioctl_sc_emerg_t);
+    ioctl!(readwrite SC_EMERG_OVERRUNS   with EC, 0x3e; ec_ioctl_sc_emerg_t);
+    ioctl!(readwrite SC_SDO_REQUEST      with EC, 0x3f; ec_ioctl_sdo_request_t);
+    ioctl!(readwrite SC_REG_REQUEST      with EC, 0x40; ec_ioctl_reg_request_t);
+    ioctl!(readwrite SC_VOE              with EC, 0x41; ec_ioctl_voe_t);
+    ioctl!(readwrite SC_STATE            with EC, 0x42; ec_ioctl_sc_state_t);
+    ioctl!(write     SC_IDN              with EC, 0x43; ec_ioctl_sc_idn_t);
+
+    ioctl!(arg       DOMAIN_SIZE         with EC, 0x44);
+    ioctl!(arg       DOMAIN_OFFSET       with EC, 0x45);
+    ioctl!(arg       DOMAIN_PROCESS      with EC, 0x46);
+    ioctl!(arg       DOMAIN_QUEUE        with EC, 0x47);
+    ioctl!(readwrite DOMAIN_STATE        with EC, 0x48;  ec_ioctl_domain_state_t);
+
+    ioctl!(readwrite SDO_REQUEST_INDEX   with EC, 0x49;  ec_ioctl_sdo_request_t);
+    ioctl!(readwrite SDO_REQUEST_TIMEOUT with EC, 0x4a;  ec_ioctl_sdo_request_t);
+    ioctl!(readwrite SDO_REQUEST_STATE   with EC, 0x4b;  ec_ioctl_sdo_request_t);
+    ioctl!(readwrite SDO_REQUEST_READ    with EC, 0x4c;  ec_ioctl_sdo_request_t);
+    ioctl!(readwrite SDO_REQUEST_WRITE   with EC, 0x4d;  ec_ioctl_sdo_request_t);
+    ioctl!(readwrite SDO_REQUEST_DATA    with EC, 0x4e;  ec_ioctl_sdo_request_t);
+
+    ioctl!(readwrite REG_REQUEST_DATA    with EC, 0x4f;  ec_ioctl_reg_request_t);
+    ioctl!(readwrite REG_REQUEST_STATE   with EC, 0x50;  ec_ioctl_reg_request_t);
+    ioctl!(readwrite REG_REQUEST_WRITE   with EC, 0x51;  ec_ioctl_reg_request_t);
+    ioctl!(readwrite REG_REQUEST_READ    with EC, 0x52;  ec_ioctl_reg_request_t);
+
+    ioctl!(write     VOE_SEND_HEADER     with EC, 0x53;  ec_ioctl_voe_t);
+    ioctl!(readwrite VOE_REC_HEADER      with EC, 0x54;  ec_ioctl_voe_t);
+    ioctl!(write     VOE_READ            with EC, 0x55;  ec_ioctl_voe_t);
+    ioctl!(write     VOE_READ_NOSYNC     with EC, 0x56;  ec_ioctl_voe_t);
+    ioctl!(readwrite VOE_WRITE           with EC, 0x57;  ec_ioctl_voe_t);
+    ioctl!(readwrite VOE_EXEC            with EC, 0x58;  ec_ioctl_voe_t);
+    ioctl!(readwrite VOE_DATA            with EC, 0x59;  ec_ioctl_voe_t);
+
+    ioctl!(write      SET_SEND_INTERVAL     with EC, 0x5a; usize);
+    ioctl!(write      SC_OVERLAPPING_IO     with EC, 0x5b; ec_ioctl_config_t);
+    ioctl!(write      SLAVE_REBOOT          with EC, 0x5c; ec_ioctl_slave_reboot_t);
+    ioctl!(readwrite  SLAVE_REG_READWRITE   with EC, 0x5d; ec_ioctl_slave_reg_t);
+    ioctl!(readwrite  REG_REQUEST_READWRITE with EC, 0x5e; ec_ioctl_reg_request_t);
+    ioctl!(read       SETUP_DOMAIN_MEMORY   with EC, 0x60; ec_ioctl_master_activate_t);
+    ioctl!(arg        DEACTIVATE_SLAVES     with EC, 0x61);
+
+    // TODO: ioctl!(arg        64_REF_CLK_TIME_QUEUE with EC, 0x62);
+    // TODO: ioctl!(read       64_REF_CLK_TIME       with EC, 0x63; u64);
+
+    ioctl!(readwrite  SC_FOE_REQUEST        with EC, 0x64; ec_ioctl_foe_request_t);
+    ioctl!(readwrite  FOE_REQUEST_FILE      with EC, 0x65; ec_ioctl_foe_request_t);
+    ioctl!(readwrite  FOE_REQUEST_TIMEOUT   with EC, 0x66; ec_ioctl_foe_request_t);
+    ioctl!(readwrite  FOE_REQUEST_STATE     with EC, 0x67; ec_ioctl_foe_request_t);
+    ioctl!(readwrite  FOE_REQUEST_READ      with EC, 0x68; ec_ioctl_foe_request_t);
+    ioctl!(readwrite  FOE_REQUEST_WRITE     with EC, 0x69; ec_ioctl_foe_request_t);
+    ioctl!(readwrite  FOE_REQUEST_DATA      with EC, 0x6a; ec_ioctl_foe_request_t);
+    ioctl!(write      RT_SLAVE_REQUESTS     with EC, 0x6b; u32);
+    ioctl!(arg        EXEC_SLAVE_REQUESTS   with EC, 0x6c);
+    ioctl!(arg        EOE_IS_OPEN           with EC, 0x6d);
+    ioctl!(arg        EOE_PROCESS           with EC, 0x6e);
+    ioctl!(arg        SEND_EXT              with EC, 0x6f);
+    ioctl!(readwrite EOE_ADDIF              with EC, 0x70; ec_ioctl_eoe_if_t);
+    ioctl!(readwrite EOE_DELIF              with EC, 0x71; ec_ioctl_eoe_if_t);
+    ioctl!(readwrite PCAP_DATA              with EC, 0x72; ec_ioctl_pcap_data_t);
+
+    // Gateway
+    ioctl!(readwrite MBOX_GATEWAY           with EC, 0x73; ec_ioctl_mbox_gateway_t);
+    ioctl!(write     SLAVE_DICT_UPLOAD      with EC, 0x7f; ec_ioctl_slave_dict_upload_t);
 }
