3f3d8640d5ca788d58fd47f5d212b3f4fe071f7e
Markus Kohlhase <markus.kohlhase@slowtec.de>
2020-10-27 19:28:48 +0100

    Add pregenerated bindings

--- ethercat-sys/build.rs
+++ ethercat-sys/build.rs
@@ -4,87 +4,89 @@
 use std::{env, fmt::Write, fs, path::PathBuf};
 
 fn main() {
-    let path = env::var("ETHERCAT_PATH").expect(
-        "Please set the ETHERCAT_PATH env var to the location of \
+    if env::var("CARGO_FEATURE_PREGENERATED_BINDINGS").is_err() {
+        let path = env::var("ETHERCAT_PATH").expect(
+            "Please set the ETHERCAT_PATH env var to the location of \
                  a checkout of the Ethercat master after running configure",
-    );
+        );
 
-    let bindings = bindgen::Builder::default()
-        .header(format!("{}/lib/ioctl.h", path))
-        .clang_arg(format!("-I{}", path))
-        .derive_default(true)
-        .derive_debug(false)
-        .prepend_enum_name(false)
-        .ignore_functions()
-        .whitelist_type("ec_ioctl_.*")
-        .whitelist_type("ec_master_state_t")
-        .whitelist_var("EC_IOCTL_.*")
-        .whitelist_var("EC_MAX_.*")
-        .layout_tests(false)
-        .generate()
-        .expect("Unable to generate bindings");
+        let bindings = bindgen::Builder::default()
+            .header(format!("{}/lib/ioctl.h", path))
+            .clang_arg(format!("-I{}", path))
+            .derive_default(true)
+            .derive_debug(false)
+            .prepend_enum_name(false)
+            .ignore_functions()
+            .whitelist_type("ec_ioctl_.*")
+            .whitelist_type("ec_master_state_t")
+            .whitelist_var("EC_IOCTL_.*")
+            .whitelist_var("EC_MAX_.*")
+            .layout_tests(false)
+            .generate()
+            .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
 
-    // Generate the EC_IOCTL_ ioctl numbers -- bindgen can't handle them.
-    let code =
-        fs::read_to_string(&format!("{}/master/ioctl.h", path)).expect("master/ioctl.h not found");
-    let mut new = String::new();
-    for line in code.split('\n') {
-        let parts = line.split_whitespace().collect::<Vec<_>>();
-        if parts.len() >= 3
-            && parts[0] == "#define"
-            && parts[1].starts_with("EC_IOCTL_")
-            && parts[2].starts_with("EC_IO")
-        {
-            let name = &parts[1]["EC_IOCTL_".len()..];
+        // Generate the EC_IOCTL_ ioctl numbers -- bindgen can't handle them.
+        let code = fs::read_to_string(&format!("{}/master/ioctl.h", path))
+            .expect("master/ioctl.h not found");
+        let mut new = String::new();
+        for line in code.split('\n') {
+            let parts = line.split_whitespace().collect::<Vec<_>>();
+            if parts.len() >= 3
+                && parts[0] == "#define"
+                && parts[1].starts_with("EC_IOCTL_")
+                && parts[2].starts_with("EC_IO")
+            {
+                let name = &parts[1]["EC_IOCTL_".len()..];
 
-            // FIXME:
-            // There are constant names that start with a number
-            // so they would be invalid rust code.
-            // The problem exists e.g. if you try to
-            // compile the v1.5.2-sncn-11 branch of the synapticon
-            // fork: https://github.com/synapticon/Etherlab_EtherCAT_Master
-            // So as a first dirty workaround we just ignore them
-            // to be able to compile.
-            if name.starts_with("64_REF_CLK") {
-                continue;
+                // FIXME:
+                // There are constant names that start with a number
+                // so they would be invalid rust code.
+                // The problem exists e.g. if you try to
+                // compile the v1.5.2-sncn-11 branch of the synapticon
+                // fork: https://github.com/synapticon/Etherlab_EtherCAT_Master
+                // So as a first dirty workaround we just ignore them
+                // to be able to compile.
+                if name.starts_with("64_REF_CLK") {
+                    continue;
+                }
+
+                let mut numparts = parts[2].split("(");
+                let access = match numparts.next().unwrap() {
+                    "EC_IO" => match name {
+                        "SEND" | "SEND_EXT" => "arg",
+                        x if x.starts_with("DOMAIN_") => "arg",
+                        _ => "none",
+                    },
+                    "EC_IOR" => "read",
+                    "EC_IOW" => "write",
+                    "EC_IOWR" => "readwrite",
+                    _ => unreachable!("invalid IO macro found"),
+                };
+                let number = numparts.next().unwrap().trim_matches(&[')', ','][..]);
+                let argtype = parts.get(3).map(|p| match p.trim_matches(')') {
+                    "uint32_t" => "u32",
+                    "uint64_t" => "u64",
+                    "size_t" => "usize",
+                    x => x,
+                });
+                write!(
+                    &mut new,
+                    "ioctl!({:10} {:20} with EC, {}{}{});\n",
+                    access,
+                    name,
+                    number,
+                    if argtype.is_some() { "; " } else { "" },
+                    argtype.unwrap_or("")
+                )
+                .unwrap();
             }
-
-            let mut numparts = parts[2].split("(");
-            let access = match numparts.next().unwrap() {
-                "EC_IO" => match name {
-                    "SEND" | "SEND_EXT" => "arg",
-                    x if x.starts_with("DOMAIN_") => "arg",
-                    _ => "none",
-                },
-                "EC_IOR" => "read",
-                "EC_IOW" => "write",
-                "EC_IOWR" => "readwrite",
-                _ => unreachable!("invalid IO macro found"),
-            };
-            let number = numparts.next().unwrap().trim_matches(&[')', ','][..]);
-            let argtype = parts.get(3).map(|p| match p.trim_matches(')') {
-                "uint32_t" => "u32",
-                "uint64_t" => "u64",
-                "size_t" => "usize",
-                x => x,
-            });
-            write!(
-                &mut new,
-                "ioctl!({:10} {:20} with EC, {}{}{});\n",
-                access,
-                name,
-                number,
-                if argtype.is_some() { "; " } else { "" },
-                argtype.unwrap_or("")
-            )
-            .unwrap();
         }
+        fs::write(out_path.join("ioctls.rs"), new.as_bytes())
+            .expect("failed to write ioctls.rs bindings");
     }
-    fs::write(out_path.join("ioctls.rs"), new.as_bytes())
-        .expect("failed to write ioctls.rs bindings");
 }
--- ethercat-sys/src/lib.rs
+++ ethercat-sys/src/lib.rs
@@ -5,7 +5,14 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(not(feature = "pregenerated-bindings"))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(all(not(feature = "sncn"), feature = "pregenerated-bindings"))]
+include!("bindings-v1.5-334c34cfd2e5.rs");
+
+#[cfg(all(feature = "sncn", feature = "pregenerated-bindings"))]
+include!("bindings-v1.5.2-sncn-11.rs");
 
 use ioctl_sys::{io, ioc, ioctl, ior, iorw, iow};
 
@@ -13,5 +20,12 @@ pub mod ioctl {
     use super::EC_IOCTL_TYPE as EC;
     use super::*;
 
+    #[cfg(not(feature = "pregenerated-bindings"))]
     include!(concat!(env!("OUT_DIR"), "/ioctls.rs"));
+
+    #[cfg(all(not(feature = "sncn"), feature = "pregenerated-bindings"))]
+    include!("ioctls-v1.5-334c34cfd2e5.rs");
+
+    #[cfg(all(feature = "sncn", feature = "pregenerated-bindings"))]
+    include!("ioctls-v1.5.2-sncn-11.rs");
 }
